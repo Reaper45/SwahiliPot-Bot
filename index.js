4a64e99d45e0d7809f9180890b4a898bd4c7b200
Chriss Mwadime <Chriss Mwadime>
2017-06-08 17:20:58 +0300

    All set and tested

---
+++ index.js
@@ -0,0 +1,17 @@
+
+var Twit = require('twit');
+var config = require('./config');
+
+var T = new Twit(config);
+
+// Stream tweets
+var stream = T.stream('statuses/filter', { track: ['#swahilipothub', '#SwahiliPotHub','SwahiliPotHub', 'swahilipothub'] });
+stream.on('tweet', function (tweet) {
+  var twitId = tweet.id;
+  var from = tweet.user.screen_name;
+
+  //Retweet a tweet
+  T.post('statuses/retweet/:id', { id: twitId }, function (err, data, response) {
+    console.log('Retweeted: '+from)
+  })
+});
